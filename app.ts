d51e0ab0965463dae0d31168c0bca35f6368886a
Nuwan Fernando <nuwaninfo@gmail.com>
2024-11-09 07:36:54 +0200

    Done exercise 2 Creating type TVehicle

--- app.ts
+++ app.ts
@@ -1,1 +1,17 @@
 console.log("Hello World!")
+
+type TVehicle = {
+    model: string,
+    color: string,
+    year: number,
+    power: number
+}
+
+let Vehicle: TVehicle  = {
+    model: "Boring generic vehicle",
+    color: "Red",
+    year: 1993,
+    power: 60 
+}
+
+console.log(Vehicle)
--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,9 @@
 "use strict";
 console.log("Hello World!");
+let Vehicle = {
+    model: "Boring generic vehicle",
+    color: "Red",
+    year: 1993,
+    power: 60
+};
+console.log(Vehicle);
