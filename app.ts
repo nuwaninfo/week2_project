edab42cfafec267489627882be4a1874f21fd2a3
Nuwan Fernando <nuwaninfo@gmail.com>
2024-11-09 08:02:03 +0200

    Done exercise 3 Extending interface

--- app.ts
+++ app.ts
@@ -15,3 +15,52 @@ let Vehicle: TVehicle  = {
 }
 
 console.log(Vehicle)
+
+interface IVehicle {
+    model: string,
+    color: string,
+    year: number,
+    power: number
+}
+
+interface ICar extends IVehicle {
+    bodyType: string,
+    wheelCount: number
+}
+
+interface IBoat extends IVehicle {
+    draft: number
+}
+
+interface IPlane extends IVehicle {
+    wingspan: number
+}
+
+let car: ICar = {
+    model: "Ford focus", 
+    color: "Green", 
+    year: 2016, 
+    power: 150, 
+    bodyType: "Hatchback", 
+    wheelCount: 4
+}
+
+let plane: IPlane = {
+    model: "Boeing 777", 
+    color: "White", 
+    year: 2020, 
+    power: 170000, 
+    wingspan: 65
+}
+
+let boat: IBoat = {
+    model: "Bella", 
+    color: "Black", 
+    year: 2022, 
+    power: 100, 
+    draft: 0.42
+}
+
+console.log(car)
+console.log(plane)
+console.log(boat)
--- dist/app.js
+++ dist/app.js
@@ -7,3 +7,28 @@ let Vehicle = {
     power: 60
 };
 console.log(Vehicle);
+let car = {
+    model: "Ford focus",
+    color: "Green",
+    year: 2016,
+    power: 150,
+    bodyType: "Hatchback",
+    wheelCount: 4
+};
+let plane = {
+    model: "Boeing 777",
+    color: "White",
+    year: 2020,
+    power: 170000,
+    wingspan: 65
+};
+let boat = {
+    model: "Bella",
+    color: "Black",
+    year: 2022,
+    power: 100,
+    draft: 0.42
+};
+console.log(car);
+console.log(plane);
+console.log(boat);
